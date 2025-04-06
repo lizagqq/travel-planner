ee16ef8b54d4f38afd4af0bfc22d39e2e373b61e
lizagqq <lisaverevkina2003@gmail.com>
2025-04-06 21:39:55 +0300

    improve profile

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -2,7 +2,7 @@
 const pool = require("../db");
 
 const getProfile = async (req, res) => {
-    const userId = req.user.userId; // Извлекаем userId из токена
+    const userId = req.user.id; // Извлекаем userId из токена
     try {
         const user = await pool.query("SELECT id, username, email, created_at FROM users WHERE id = $1", [userId]);
         if (user.rows.length === 0) {
